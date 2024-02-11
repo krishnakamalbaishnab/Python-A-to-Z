8866eb5cfefaaa2c075d337cad3ec2ada56e50e5
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-11 21:00:35 +0000

    formatted string

---
+++ formatedString.py
@@ -0,0 +1,13 @@
+firstName = "Krishna"
+middleName = "Kamal"
+lastName = "Baishnab"
+
+#so no if we want tp print in first name middle name and last name in a single line then we can use the following code
+##using formated string
+print(f"{firstName} {middleName} {lastName}")
+
+#using the format method
+print("{} {} {}".format(firstName, middleName, lastName))
+
+#using the concatenation
+print(firstName + " " + middleName + " " + lastName)
