0cacc77d9f04346701d3258578a1209612e39024
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-15 09:32:12 +0000

    defaultArguments

---
+++ defaultArguments.py
@@ -0,0 +1,9 @@
+def increment(number, by=1):
+    return number + by
+
+# print(increment(2, 5))
+
+
+# we can simply call the function without passing the second argument and it will take the default value of the second argument
+
+print(increment(2, by=5))
