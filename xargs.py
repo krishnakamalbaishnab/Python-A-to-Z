790e71340dcbde76c694be7ca1601bd39ed9f670
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-20 08:51:04 +0000

    functional arguments

---
+++ xargs.py
@@ -0,0 +1,13 @@
+def multiply(*numbers):
+    # total = 1
+    for number in numbers:
+        print(number)
+        # total *= number
+    # return number
+
+# print(multiply(2, 3, 4, 5))
+        
+multiply(2, 3, 4, 5)
+
+
+
