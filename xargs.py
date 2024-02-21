e35f777196abaeda14ded7528ee678f77c9d51d7
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-21 09:12:22 +0000

    singlearguments

--- xargs.py
+++ xargs.py
@@ -1,9 +1,10 @@
 def multiply(*numbers):
-    # total = 1
+    total = 1
     for number in numbers:
-        print(number)
-        # total *= number
+        # print(number)
+        total *= number
     # return number
+    print(f"The total of the {numbers}is {total}")
 
 # print(multiply(2, 3, 4, 5))
         
