2107368b2f1db4573bd51b6ee0ac69d496d9e121
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-02-21 09:24:21 +0000

    scope Local and Global

---
+++ scopeLocalAndGlobalVariable.py
@@ -0,0 +1,9 @@
+message = "a" # this is a global variable
+
+
+def greet_user():
+    # """Display a simple greeting."""
+    global message # this is a global variable , this will modify the global variable
+    message = "b" # this is a local variable
+
+greet_user()
