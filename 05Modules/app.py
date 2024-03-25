832a47e3cae8b32c6e3ff09ce360d5a1bc79cf2b
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-03-25 19:53:14 +0000

    Import notes

--- 05Modules/app.py
+++ 05Modules/app.py
@@ -17,3 +17,9 @@ sales.calculate_tax()
 #the difference is that you have to use the module name before the function name
 
 #you can also import the module as an alias
+
+
+"""
+Study about the following modules:
+relative import and absolute import
+""
