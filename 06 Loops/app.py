3e7708f2ed2114f9f850891d9f6066a0dff2673d
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-01-05 17:13:43 +0000

    pattern

--- 06 Loops/app.py
+++ 06 Loops/app.py
@@ -1,6 +1,27 @@
-prices = [10,20,30]
-sum = 0
-for i in prices:
-    sum = sum + i
-print(sum)
+# prices = [10,20,30]
+# sum = 0
+# for i in prices:
+#     sum = sum + i
+# print(sum)
 
+
+
+#print the following pattern
+
+
+# XXXXX
+# XX
+# XXXXX
+# XX
+# XX
+# XX
+
+
+pattern=[5,2,5,2,2,2]
+
+
+for xCount in pattern:
+    output = ''
+    for count in range(xCount):
+        output = output+'X'
+    print(output)
