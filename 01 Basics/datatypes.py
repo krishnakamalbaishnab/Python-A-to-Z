af9d61c203d1db8bcb6b300c521c1e4856ff9fb8
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-10 20:35:22 +0000

    Data Types

---
+++ 01 Basics/datatypes.py
@@ -0,0 +1,102 @@
+# #data types in python
+
+# 1. None
+# 2. numeric
+# 3. List
+# 4. Tupple
+# 5. set
+# 6. String
+# 7. Range
+# 8. Dctionary
+
+
+
+# Numeric data types
+
+# int 
+# float
+# complex
+# bool
+
+
+
+#list example
+
+list1 = ['ram', 'krishna', 'hori'] #list of name of people
+
+#tuple example  : Tuples are immutable can not be modified after it;s been created
+
+tuple1 = (1,2,3,4,5) #tuple of numbers
+
+tuple2 = (4) #this is not tuple we need commna to be a tuple so the correct syntax is 
+
+tuple3 = (4,)
+
+
+#set example 
+
+set1 = {2,3,4,5,56}
+
+# Using set() Constructor
+
+s = set([10, 20, 30, 10, 20])  # Duplicates are removed
+print(s)  # Output: {10, 20, 30}
+
+# Empty Set (Use set(), NOT {})
+
+s = set()  # Correct: Creates an empty set
+s2 = {}    # Incorrect: Creates an empty dictionary
+
+
+
+#dictionary
+
+
+
+
+# Using {} (Curly Braces)
+
+student = {"name": "Krishna", "age": 25, "city": "Edinburgh"}
+print(student)  # Output: {'name': 'Krishna', 'age': 25, 'city': 'Edinburgh'}
+
+
+
+
+student = dict(name="Krishna", age=25, city="Edinburgh")
+print(student)  # Output: {'name': 'Krishna', 'age': 25, 'city': 'Edinburgh'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
