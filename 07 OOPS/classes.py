29a3511dde25eea448ffcf0a149212121b9cdade
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-16 19:18:37 +0000

    Classes and Objects

---
+++ 07 OOPS/classes.py
@@ -0,0 +1,11 @@
+class Persona:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
+
+# Example usage
+p = Persona("Krishna", 25)
+print(p.introduce())
