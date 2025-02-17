c4729640477964f84736128a803b44c0c5a1c946
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-17 17:22:33 +0000

    constructors in python

---
+++ 07 OOPS/constructors.py
@@ -0,0 +1,10 @@
+class MyClass:
+    def __init__(self, param1, param2):
+        self.attribute1 = param1
+        self.attribute2 = param2
+
+# Creating an object of the class
+obj = MyClass("Hello", 42)
+
+print(obj.attribute1)  # Output: Hello
+print(obj.attribute2)  # Output: 42
